deb92e17b3416a6cc73831d00429e8c678bf702e
sergeynovik123 <65292367+sergeynovik123@users.noreply.github.com>
2020-05-20 01:32:06 +0300

    Update printf.asm

--- printf.asm
+++ printf.asm
@@ -36,97 +36,97 @@ printf:			push ebp
 			mov ecx, [ebp + 8]
 			add ebp, 12
 
-nextsimb:		cmp byte [ecx], 0		;
-			je endstr			;
-			cmp byte [ecx], '%'		;		
-			je format_percent		;single  		
-			mov eax, 4			;char
-			mov ebx, 1			;processing	
-			mov edx, 1			;		
-			int 0x80			;		
-			inc ecx				;		
-			jmp nextsimb			;
+nextsimb:		cmp byte [ecx], 0			;
+			je endstr				;
+			cmp byte [ecx], '%'			;		
+			je format_percent			;single  		
+			mov eax, 4				;char
+			mov ebx, 1				;processing	
+			mov edx, 1				;		
+			int 0x80				;		
+			inc ecx					;		
+			jmp nextsimb				;
 												
-format_percent:		mov ebx, jmp_table		;
-							;
-			inc ecx				;
-			cmp byte[ecx], '%'		;percent
-			je percent 			;processing
-			cmp byte[ecx], 'b'		;
-			jb wrongsimb			;
-			cmp byte [ecx], 'x'		;
-			ja wrongsimb			;
+format_percent:		mov ebx, jmp_table			;
+								;
+			inc ecx					;
+			cmp byte[ecx], '%'			;percent
+			je percent 				;processing
+			cmp byte[ecx], 'b'			;
+			jb wrongsimb				;
+			cmp byte [ecx], 'x'			;
+			ja wrongsimb				;
 
 
-			mov al, 'b'			;
-			xor edx, edx			;
-next:			cmp al, byte[ecx]		;finding
-			je found_simb			;case in
-			inc al				;jmp table
-			inc edx				;
+			mov al, 'b'				;
+			xor edx, edx				;
+next:			cmp al, byte[ecx]			;finding
+			je found_simb				;case in
+			inc al					;jmp table
+			inc edx					;
 			jmp next
 
 
-found_simb:		push ecx			;
-			jmp [ebx + edx * PTR_SIZE]	;jmp to func
+found_simb:		push ecx				;
+			jmp [ebx + edx * PTR_SIZE]		;jmp to func
 
 
-str:			mov ecx, [ebp]			;
-			call printstring		; %s
-			jmp printed 			;
+str:			mov ecx, [ebp]				;
+			call printstring			; %s
+			jmp printed 				;
 
-char:			mov ecx, [ebp]			;
-			mov ebx, bufchar		;
-			mov byte [ebx], cl 		;
-			mov ecx, bufchar		; %c
-			call printchar			;
-			jmp printed 			;
-
-
-dec:			mov ecx, [ebp]			;
-			mov ebx, 10			; %d
-			call printdec			;
-			jmp printed 			;
+char:			mov ecx, [ebp]				;
+			mov ebx, bufchar			;
+			mov byte [ebx], cl 			;
+			mov ecx, bufchar			; %c
+			call printchar				;
+			jmp printed 				;
 
 
-hex:			mov ecx, [ebp]			;
-			mov ebx, 16			; %x
-			call print_pow2			;
-			jmp printed 			;
+dec:			mov ecx, [ebp]				;
+			mov ebx, 10				; %d
+			call printdec				;
+			jmp printed 				;
 
 
-oct:			mov ecx, [ebp]			;
-			mov ebx, 8			; %o
-			call print_pow2			;
-			jmp printed 			;
+hex:			mov ecx, [ebp]				;
+			mov ebx, 16				; %x
+			call print_pow2				;
+			jmp printed 				;
 
 
-bin:			mov ecx, [ebp]			;	
-			mov ebx, 2			; %b
-			call print_pow2 		;
-			jmp printed 			;
+oct:			mov ecx, [ebp]				;
+			mov ebx, 8				; %o
+			call print_pow2				;
+			jmp printed 				;
 
 
-percent:		mov eax, 4			;
-			mov ebx, 1			; %%
-			mov edx, 1			;
-			int 0x80			;
-			jmp simbprinted			;
+bin:			mov ecx, [ebp]				;	
+			mov ebx, 2				; %b
+			call print_pow2 			;
+			jmp printed 				;
 
-printed: 		add ebp, 4			;go to next param in stack
-			pop ecx				;
-			jmp simbprinted			;
 
-wrongsimb:		mov eax, 4			;
-			mov ebx, 1			; 
-			mov edx, 1			;
-			int 0x80			;
+percent:		mov eax, 4				;
+			mov ebx, 1				; %%
+			mov edx, 1				;
+			int 0x80				;
+			jmp simbprinted				;
+	
+printed: 		add ebp, 4				;go to next param in stack
+			pop ecx					;
+			jmp simbprinted				;
+
+wrongsimb:		mov eax, 4				;
+			mov ebx, 1				; 
+			mov edx, 1				;
+			int 0x80				;
 				
 
-			jmp endstr			;
+			jmp endstr				;
 
-simbprinted:		inc ecx				;
-			jmp nextsimb			;
+simbprinted:		inc ecx					;
+			jmp nextsimb				;
 
 endstr:			pop ebp							
 			ret
@@ -223,90 +223,90 @@ print_pow2:		cmp ecx, 0
 			je .zero
 			mov edi, str_bin_pow
 
-			test ecx, 80000000h				;
-			je .positive					; if > 0
-												
-			push ecx					;
-			push ebx					;
+			test ecx, 80000000h			;
+			je .positive				; if > 0
+											
+			push ecx				;
+			push ebx				;
 				
-			mov eax, 4					;
-			mov ebx, 1					;
-			mov ecx, minus					;print '-'
-			mov edx, 1					;
-			int 80h 					;
+			mov eax, 4				;
+			mov ebx, 1				;
+			mov ecx, minus				;print '-'
+			mov edx, 1				;
+			int 80h 				;
 				
-			pop ebx						;
-			pop ecx						;
+			pop ebx					;
+			pop ecx					;
 
-			neg ecx						;ecx *= -1
+			neg ecx					;ecx *= -1
 
 .positive:		mov eax, ecx
 
-			cmp ebx, 16					;
-			je .hex						;finding 
-			cmp ebx, 2					;base of 
-			je .bin 					;number
-			cmp ebx, 8					;system
-			je .oct 					;
+			cmp ebx, 16				;
+			je .hex					;finding 
+			cmp ebx, 2				;base of 
+			je .bin 				;number
+			cmp ebx, 8				;system
+			je .oct 				;
 
 			jmp .printed
 
-.bin:			mov ecx, 1					;ecx - n bits
-			mov esi, 1b 					;shift
-			xor ebx, ebx					;esi last N(pow 2) bits
-			jmp .next_simb					;
+.bin:			mov ecx, 1				;ecx - n bits
+			mov esi, 1b 				;shift
+			xor ebx, ebx				;esi last N(pow 2) bits
+			jmp .next_simb				;
 
-.oct:			mov ecx, 3					;
-			mov esi, 111b 					;
-			xor ebx, ebx					;
-			jmp .next_simb					;
+.oct:			mov ecx, 3				;
+			mov esi, 111b 				;
+			xor ebx, ebx				;
+			jmp .next_simb				;
 
-.hex:			mov ecx, 4					;
-			mov esi, 1111b					;
-			xor ebx, ebx 					;
-			jmp .next_simb 					;
-
-
+.hex:			mov ecx, 4				;
+			mov esi, 1111b				;
+			xor ebx, ebx 				;
+			jmp .next_simb 				;
 
 
-.next_simb:		cmp eax, 0					;
-			je .endnumb					;
-			mov edx, eax					;put
-			and edx, esi 					;chars 
-			mov [edi + ebx], dl				;in 
-			inc ebx						;buf
-			shr eax, cl 					;
-			jmp .next_simb					;
+
+
+.next_simb:		cmp eax, 0				;
+			je .endnumb				;
+			mov edx, eax				;put
+			and edx, esi 				;chars 
+			mov [edi + ebx], dl			;in 
+			inc ebx					;buf
+			shr eax, cl 				;
+			jmp .next_simb				;
 
 .endnumb		mov ecx, ebx
 			dec edi				
 
-.printnumbs:		push ecx					;
-			add ecx, edi					;conversing
-			cmp byte[ecx], 10				;to 
-			jb .decimal					;ascii
-			add byte[ecx], 'A' - 10				;
-			jmp .print 					;
+.printnumbs:		push ecx				;
+			add ecx, edi				;conversing
+			cmp byte[ecx], 10			;to 
+			jb .decimal				;ascii
+			add byte[ecx], 'A' - 10			;
+			jmp .print 				;
 
-.decimal		add byte[ecx], '0'				;
+.decimal		add byte[ecx], '0'			;
 				
-.print			mov eax, 4					;
-			mov ebx, 1					;print
-			mov edx, 1					;simbs
-			int 80h 					;
-			pop ecx						;
-			loop .printnumbs				;
+.print			mov eax, 4				;
+			mov ebx, 1				;print
+			mov edx, 1				;simbs
+			int 80h 				;
+			pop ecx					;
+			loop .printnumbs			;
 
-			jmp .printed 					;
+			jmp .printed 				;
 
 
 
-.zero:			mov ecx, simbol	 				;
-			mov byte[ecx], '0'				;
-			mov eax, 4					;if input 
-			mov ebx, 1					;number == 0
-			mov edx, 1					;
-			int 80h 					;
+.zero:			mov ecx, simbol	 			;
+			mov byte[ecx], '0'			;
+			mov eax, 4				;if input 
+			mov ebx, 1				;number == 0
+			mov edx, 1				;
+			int 80h 				;
 
 
 .printed:		ret
@@ -320,36 +320,36 @@ section			.data
 
 
 
-newtest:		db "I %% %s %x %d%%%c %b", 10, 0		;
-test1:			db "%x, yes, you, %s ", 0ah, 0			;format
-test2:			db "%b %c ARRRRRRR ", 0ah, 0			;strings
-test3:			db "%x %o %b %d", 0ah, 0			;
-dedtest:		db "I %s %x. %d%%%b", 10, 0			;
+newtest:		db "I %% %s %x %d%%%c %b", 10, 0	;
+test1:			db "%x, yes, you, %s ", 0ah, 0		;format
+test2:			db "%b %c ARRRRRRR ", 0ah, 0		;strings
+test3:			db "%x %o %b %d", 0ah, 0		;
+dedtest:		db "I %s %x. %d%%%b", 10, 0		;
 
 
 
 
 
-number			dd 100d						;
-love:			db "LOVE", 0					;
-Char:			db 'W'						;
-Msg:			db "Hey, You", 0				;	
-string:			db "HELLO", 0					;
+number			dd 100d					;
+love:			db "LOVE", 0				;
+Char:			db 'W'					;
+Msg:			db "Hey, You", 0			;	
+string:			db "HELLO", 0				;
 
 
 
 
 
 
-jmp_table:		dd bin,						;%b
-			dd char, 					;%c
-			dd dec,						;%d
+jmp_table:		dd bin,					;%b
+			dd char, 				;%c
+			dd dec,					;%d
 			times ('o' - 'd' - 1) dd wrongsimb,
-			dd oct,						;%o
+			dd oct,					;%o
 			times ('s' - 'o' - 1) dd wrongsimb,  
-			dd str,						;%s
+			dd str,					;%s
 			times ('x' - 's' - 1) dd wrongsimb,  
-			dd hex						;%x
+			dd hex					;%x
 
 
 
