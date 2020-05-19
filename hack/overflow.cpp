fe626b7e483b4606e03ae9ecf47505f76b510de9
sergeynovik123 <65292367+sergeynovik123@users.noreply.github.com>
2020-05-20 01:35:39 +0300

    Update and rename overlow.cpp to overflow.cpp

--- hack/overflow.cpp
+++ hack/overflow.cpp
@@ -6,36 +6,32 @@ const int RET_ADDR = 0xFFFC;
 const int BUF_ADDR = 0x0166;
 const int truehash = 7087220;
 
-int hash_file(FILE* prog);
+bool hash_file(FILE* prog);
 	
 
 int main(){
 
 	FILE* prog = fopen("W:\\TOBEHACK.com", "r");
 	assert(prog);
-	int hash = hash_file(prog);
-	if(hash != truehash){
-		printf("WRONG FILE");
+	if(!check_hash_file(prog))
 		return 0;
-	}
 
 	FILE* output = fopen("W:\\hack.txt", "w");
 	assert(output);
 	int dif = RET_ADDR - BUF_ADDR + 3;
 
-	char buf[dif] = {};
-
+	char buf = new char[dif];
 	buf[dif - 3] = 0x14;	
 	buf[dif - 2] = 0x01;	
 	buf[dif - 1] = '\r';	
-
+	
 	fwrite(buf, 1, dif, output);
 	printf("All done\n");
 
 	fclose(output); 
 }
 
-int hash_file(FILE* prog){
+bool hash_file(FILE* prog){
 	assert(prog);
 
 	fseek(prog, 0, SEEK_END);
@@ -49,5 +45,9 @@ int hash_file(FILE* prog){
 	for(int i = 0; i < file_size; ++i)
 		hash += buff[i] * i * 31;
 	
-	return hash;
+	if(hash != truehash){
+		printf("WRONG FILE");
+		return false;
+	}
+	return true;
 }
